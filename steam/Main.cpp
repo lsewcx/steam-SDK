74d87940696c05b16c6c5378bfbf46c50e7ed238
lsewcx <lse210085@163.com>
2023-09-04 14:14:43 +0800

    nullptr

--- steam/Main.cpp
+++ steam/Main.cpp
@@ -21,30 +21,35 @@ uint64_t GetCurrentPlayerSteamID()//获取当前steam用户的ID
 
 int main()
 {
-    if (SteamAPI_RestartAppIfNecessary(480))
-    {
-        return 1;
+    while (1) {
+        if (SteamAPI_RestartAppIfNecessary(480))
+        {
+            return 1;
+        }
+        if (!SteamAPI_Init())
+        {
+            printf("Fatal Error - Steam must be running to play this game (SteamAPI_Init() failed).\n");
+            return 1;
+        }
+        else
+        {
+            //startfreinds();
+            ISteamUserStats* steamUserStats = SteamUserStats();//新建
+            /*手动形式
+            Achievement* achievement = new Achievement();
+            achievement->Unlockachievements(name, steamUserStats);
+            achievement->GetUserAchievementAndUnlockTime(GetCurrentPlayerSteamID(), name, achieved, unlockTime, steamUserStats);
+            delete achievement;
+            */
+            //析构函数自动调用形式
+            //achievement.Unlockachievements(name, steamUserStats);
+            //publicname.achievement.GetUserAchievementAndUnlockTime(GetCurrentPlayerSteamID(), publicname.get_name(), publicname.get_achieved(), publicname.get_unlockTime(), steamUserStats);
+            //cout << publicname.utils.getlanguage()<<endl;
+            cout << publicname.utils.getIP() << endl;
+        }
+        if (_kbhit()) {//如果有按键按下，则_kbhit()函数返回真
+            int ch = _getch();//使用_getch()函数获取按下的键值
+            if (ch == 49) { break; }//当按下1时跳出循环
+        }
     }
-    if (!SteamAPI_Init())
-    {
-        printf("Fatal Error - Steam must be running to play this game (SteamAPI_Init() failed).\n");
-        return 1;
-    }
-    else
-    {
-        //startfreinds();
-        ISteamUserStats* steamUserStats = SteamUserStats();//新建
-        /*手动形式
-        Achievement* achievement = new Achievement();
-        achievement->Unlockachievements(name, steamUserStats);
-        achievement->GetUserAchievementAndUnlockTime(GetCurrentPlayerSteamID(), name, achieved, unlockTime, steamUserStats);
-        delete achievement;
-        */
-        //析构函数自动调用形式
-        //achievement.Unlockachievements(name, steamUserStats);
-        //publicname.achievement.GetUserAchievementAndUnlockTime(GetCurrentPlayerSteamID(), publicname.get_name(), publicname.get_achieved(), publicname.get_unlockTime(), steamUserStats);
-        //cout << publicname.utils.getlanguage()<<endl;
-        cout << publicname.utils.getIP() << endl;
-    }
-    return 0;
 }
--- steam/head/public.h
+++ steam/head/public.h
@@ -3,12 +3,13 @@
 #include"achievement.cpp"
 #include"friends.cpp"
 #include"Utils.cpp"
+#include<conio.h>//获取键盘按下事件
 class Public {
 private:
 	bool achievementStatus;
 	const char* name = "ACH_WIN_ONE_GAME";//成就的api
 	uint32 unlockTime;//解锁时间
-	const char* langueage;
+	const char* langueage = nullptr;
 	bool achieved;
 public:
 	Achievement achievement;
